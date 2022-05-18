e939535957deed61abe19b6520f7fc6de0d8c3be
gsoosk <farzadhabibii98@gmail.com>
2022-05-17 17:39:41 -0700

    add postgres usage in java program

--- README.md
+++ README.md
@@ -4,5 +4,15 @@
 ### Step 1:
 running postgres nodes:
 ```shell
-docker-compose up
+docker-compose up -d
+```
+
+### Step 2:
+building code:
+```shell
+mvn clean install
+```
+running each node:
+```shell
+mvn spring-boot:run -Dspring-boot.run.profiles=<leader/node1/node2/node3>
 ```
--- src/main/java/com/example/replicatedpostgres/ReplicatedPostgresApplication.java
+++ src/main/java/com/example/replicatedpostgres/ReplicatedPostgresApplication.java
@@ -1,13 +1,53 @@
 package com.example.replicatedpostgres;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class ReplicatedPostgresApplication {
+public class ReplicatedPostgresApplication implements CommandLineRunner {
+
+    private static Logger LOG = LoggerFactory
+            .getLogger(ReplicatedPostgresApplication.class);
 
     public static void main(String[] args) {
+        LOG.info("Starting the application");
         SpringApplication.run(ReplicatedPostgresApplication.class, args);
+        LOG.info("Application finished");
+    }
+
+
+    @Autowired
+    private Environment environment;
+
+
+    @Override
+    public void run(String... args) {
+        LOG.info("EXECUTING : command line runner");
+
+        for (int i = 0; i < args.length; ++i) {
+            LOG.info("args[{}]: {}", i, args[i]);
+        }
+
+        LOG.info("Active profile is {}", this.environment.getActiveProfiles()[0]);
+        String activeProfile = this.environment.getActiveProfiles()[0];
+        if (activeProfile.equals("leader")) {
+            // TODO: run leader
+        }
+        else if (activeProfile.equals("node1")) {
+            // TODO: run node1
+        }
+        else if (activeProfile.equals("node2")) {
+            // TODO: run node2
+        }
+        else if (activeProfile.equals("node3")) {
+            // TODO: run node3
+        }
+
     }
 
 }
